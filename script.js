f10cf9fd3ead514468ca08ab19f0d36de96b4f99
Kaitlin O'Connor <globalsolidaritytiktok@gmail.com>
2022-05-24 15:46:45 -0500

    subtitles on all pics in site

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 
-const portfolioitems = document.querySelectorAll('portfolio-itme-wrapper')
